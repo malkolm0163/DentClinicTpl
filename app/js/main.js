6fbf20d5521a6d8a99f36794a9cda5fca17d4548
Malkolm0163 <Malkolm063@yandex.ru>
2016-09-29 22:07:19 +0400

    4sections done

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    console.log('asd');
+    var owl1 = $(".survey-slider");
+    owl1.owlCarousel({
+        items : 5,
+        scrollPerPage: true,
+        pagination: true,
+        slideSpeed: 800
+    });
+    var owl = owl1.data('owlCarousel');
+    $('.fa-nav').filter('.next').click(function () {
+        owl.next();
+    });
+    $('.fa-nav').filter('.prev').click(function () {
+        owl.prev();
+    });
+});
