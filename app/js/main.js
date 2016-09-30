0bd45cf66d2440c0c786365e5a2415b0da69572f
Malkolm0163 <Malkolm063@yandex.ru>
2016-09-30 13:13:41 +0400

    5sections done

--- app/js/main.js
+++ app/js/main.js
@@ -14,8 +14,63 @@ $(document).ready(function () {
     $('.fa-nav').filter('.prev').click(function () {
         owl.prev();
     });
-    // var ch = $('.works-slider').find('.item').children();
-    // console.log(ch);
-    // // ch.equalHeights();
-    // $('.item-1').equalHeights();
+
+
+    var items = $('.works-slider').find('.items');
+    items.owlCarousel({
+        items: 1,
+        pagination: false,
+        navigation: true,
+        slideSpeed: 800,
+        addClassActive: true,
+        afterMove: function () {
+            var i = items.find('.owl-item').filter('.active').children();
+            var classname = '.' + i.attr('class').substr(5);
+            var wa = $('.work-items').children();
+            wa.filter('.active').removeClass('active');
+            wa.filter(classname).addClass('active');
+        },
+        navigationText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ]
+    });
+    var owl2 = items.data('owlCarousel');
+    var owl2Items = $('.work-items').children();
+    owl2Items.click(function () {
+        var th = $(this);
+        if(th.hasClass('active')) return false;
+        var classnames = th.attr('class');
+        var regex = /(expensive|complicated|interesting|unusual)/g;
+        var m = regex.exec(classnames);
+        if (m===null) return false;
+        owl2Items.filter('.active').removeClass('active');
+        var newActive = '.' + m[0];
+        owl2Items.filter(newActive).addClass('active');
+        switch (m[0]){
+            case 'expensive':{
+                owl2.goTo(0, 200);
+                break;
+            }
+            case 'complicated':{
+                owl2.goTo(1, 200);
+                break;
+            }
+            case 'interesting':{
+                owl2.goTo(2, 200);
+                break;
+            }
+            case 'unusual':{
+                owl2.goTo(3, 200);
+                break;
+            }
+            default:{
+                console.error('Not Found');
+            }
+        }
+
+        return false;
+    });
+
+
 });
