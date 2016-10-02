fff2c38ee1f8a1b372ccd4182587b4878ab10899
malkolm063 <Malkolm163@gmail.com>
2016-10-02 21:05:05 +0400

    ready

--- app/js/main.js
+++ app/js/main.js
@@ -143,4 +143,15 @@ $(document).ready(function () {
     });
     var sliderOwl = slider.data('owlCarousel');
     sliderOwl.goTo(1);
+    $('a[href="#popup-form"]').magnificPopup({
+        closeBtnInside: true
+    });
+
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
+    });
 });
